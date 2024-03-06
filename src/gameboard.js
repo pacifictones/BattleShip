916b074e0db1ddd96fdcebf0b497df79ffe1af5e
pacifictones <brianwburk@gmail.com>
2024-03-06 16:28:45 +0400

    attack method now updates correct ship using it's own hit method

--- src/gameboard.js
+++ src/gameboard.js
@@ -13,6 +13,7 @@ class Gameboard {
   placeShip(len, y, x, direction) {
     const coords = [];
     const newShip = new Ship(len);
+
     for (let i = 0; i < len; i++) {
       this.board[y][x] = "ship";
       coords.push([y, x]);
@@ -29,15 +30,27 @@ class Gameboard {
 
   receiveAttack(y, x) {
     if (this.board[y][x] === "ship") {
+      for (let i = 0; i < this.ships.length; i++) {
+        for (let j = 0; j < this.ships[i].coords.length; j++) {
+          if (
+            JSON.stringify(this.ships[i].coords[j]) === JSON.stringify([y, x])
+          ) {
+            this.ships[i].newShip.hit();
+            this.ships[i].newShip.isSunk();
+          }
+        }
+      }
       this.board[y][x] = "hit";
+      //   this.ships[0].newShip.hit();
+    } else if (this.board[y][x] === null) {
+      this.board[y][x] = "miss";
     }
   }
 }
 
 module.exports = Gameboard;
 const board = new Gameboard();
-board.placeShip(2, 0, 1);
-console.log(board.ships);
-console.log(board);
+board.placeShip(2, 0, 1, 1);
+board.receiveAttack(0, 2);
 board.receiveAttack(0, 1);
-console.log(board);
+console.log(board.ships);
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -25,3 +25,27 @@ test("Test Gameboard is receiveAttack is working", () => {
 
   expect(newBoard.board[0][1]).toBe("hit");
 });
+
+test("Test receiveAttack incrementing hits", () => {
+  const newBoard = new Gameboard();
+  newBoard.placeShip(2, 0, 1, 2);
+  newBoard.receiveAttack(0, 1);
+
+  expect(newBoard.ships[0].newShip.hits).toBe(1);
+});
+
+test("Test Gameboard is receiveAttack miss is working", () => {
+  const newBoard = new Gameboard();
+  newBoard.placeShip(2, 0, 1, 2);
+  newBoard.receiveAttack(2, 1);
+
+  expect(newBoard.board[2][1]).toBe("miss");
+});
+
+test("Test receiveAttack runs hit method on appropriate ship", () => {
+  const newBoard = new Gameboard();
+  newBoard.placeShip(2, 0, 1, 2);
+  newBoard.receiveAttack(0, 1);
+
+  expect(newBoard.ships[0].newShip.hits).toEqual(1);
+});
